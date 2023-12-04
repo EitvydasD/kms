c56d590c413e2fc73a4d100e09322054dc3e2c57
Eitvydas Dzetaveckas <eitvydasas10@gmail.com>
2023-12-04 14:48:27 +0200

    .

--- KMS.Core/Services/Trip/TripService.cs
+++ KMS.Core/Services/Trip/TripService.cs
@@ -64,11 +64,11 @@ public class TripService : ITripService
     {
         var trip = await GetTrip(tripId, cancellationToken);
 
-        trip.DriverId = trip.DriverId;
-        trip.DepartedAt = trip.DepartedAt;
-        trip.ArrivedAt = trip.ArrivedAt;
-        trip.Status = trip.Status;
-        trip.Responsible = trip.Responsible;
+        trip.DriverId = request.DriverId;
+        trip.DepartedAt = request.DepartedAt;
+        trip.ArrivedAt = request.ArrivedAt;
+        trip.Status = request.Status;
+        trip.Responsible = request.Responsible;
 
         await TripRepo.SaveChangesAsync(cancellationToken);
         
